a90538d4ca048f79c2535b408f3747ab28dde6cf
Lance R. Owen, PhD <44533523+lancerowen23@users.noreply.github.com>
2025-12-02 14:36:39 -0700

    Update  Vienna_Pre_Work.R

---  Vienna_Pre_Work.R
+++  Vienna_Pre_Work.R
@@ -1,19 +1,25 @@
-### Vienna R Module Pre-Work
+### Vienna Module (Data Analytics in R) Pre-Work
 ### Author: Lance Owen
+### Date: 2 December 2025
 ### Track: Advanced
+
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
 
 # Import data
 df <- read.csv('https://raw.githubusercontent.com/kwartler/teaching-datasets/refs/heads/main/WA_Fn-UseC_-Telco-Customer-Churn.csv')
 
-### Quick view of df structure
+### QUICK REVIEW OF OF STRUCTURE, DATA TYPES, ETC.
 
+View(df)
 str(df)
 glimpse(df)
 colnames(df)
 
-### EDA of df
+### EXPLORATORY DATA ANALYSIS
 
-#quick correlation plots
+#quick correlation plots for numeric columns
 df %>% select(tenure, MonthlyCharges, TotalCharges) %>% pairs()
 
 #Better correlation plots :-)
@@ -43,8 +49,47 @@ df %>%
   scale_y_continuous(labels = percent_format()) +
   labs(title = "Churn Proportion by Contract Type")
 
-### Inspect Missing Data
+### INSPECT MISSING DATA 
 colSums(is.na(df))
+
+#drop NA rows (see 11 in TotalCharges)
+
+#see rows with NA values
+df_na <- df[rowSums(is.na(df)) > 0, ]
+View(df_na)
+
+#drop those 11 rows
+df <- na.omit(df)
+
+#check to see new df structure to ensure 11 rows were dropped (from 7043 to 7032)
+str(df)
+
+#check numerical columns for outliers using summary stats
+df_num <- df %>% select(tenure, MonthlyCharges, TotalCharges)
+summary(df_num)
+
+#check histograms
+df_num %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "value") %>%
+  ggplot(aes(x = value)) +
+  geom_histogram(bins = 30, fill = "skyblue", color = "black") +
+  facet_wrap(~variable, scales = "free") +
+  theme_minimal() +
+  labs(title = "Histograms of Tenure, Monthly Charges, and Total Charges")
+
+#check again with boxplots
+num_long <- df_num %>%
+  pivot_longer(cols = everything(),
+               names_to = "variable",
+               values_to = "value")
+ggplot(num_long, aes(x = "", y = value)) +  # x="" to create a single box per variable
+  geom_boxplot(fill = "lightblue", outlier.color = "red", outlier.shape = 8) +
+  facet_wrap(~variable, scales = "free") +  # one boxplot per variable
+  theme_minimal() +
+  labs(title = "Boxplots of Tenure, Monthly Charges, and Total Charges",
+       x = "",
+       y = "Value")
+#from this, I don't really see any outliers in numerical values
 
 # Get tallys for character and categorical columns
 tally_cols <- names(df)[sapply(df, is.character)]
